4c880f27314e240c44d5b71486f21b15c4610c17
Daniel Enrique <dcordb97@gmail.com>
2019-07-02 15:25:12 -0400

    changed title

--- src/views/Main.java
+++ src/views/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
-        primaryStage.setTitle("API Compilers");
+        primaryStage.setTitle("CompilersFX");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
